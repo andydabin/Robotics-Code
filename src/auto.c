71276970f182bf7680811885664e8f19fd63cce6
4ntLu0 <anthonyluo100@gmail.com>
2018-11-21 21:26:40 -0500

    Update auto.c
    21:26 Nov 11, 2018

--- src/auto.c
+++ src/auto.c
@@ -85,18 +85,82 @@ void driveStraight( int travelDistanceA, int travelDistanceD, int maxSpeed ){ //
   }
 }
 
-void pointTurn( int targetDegrees, int maxSpeed ) {
-
+void pointTurn( int turnType, int maxSpeed) {
+//turn type 1: 90 degree ccw, 2: 90 degree cw, 3: small ccw, 4: small cw.
   encoderReset( driveLeftOse );
   encoderReset( driveRightOse );
   gyroReset( gyro );
 
-  int targetTicks = targetDegrees; //add math here
-  int ticksA = targetTicks * 2 / 3;
-  int ticksD = targetTicks - ticksA;
+  int gyroTarget;
+  int oseTarget;
+  int oseLeftTarget;
+  int oseRightTarget;
+  int leftTargetDistance;
+  int rightTargetDistance;
+  int leftPercent;
+  int rightPercent;
+  bool turnComplete = false;
 
-  if( targetDegrees < 0 ) {//target < 0 means that we want to be turning left
+  double Ldeviation, Rdeviation;
+  int leftPower, rightPower;
+
+  if( turnType == 1 ) {
+    //Anni
+    gyroTarget = 900;
+    oseLeftTarget = -130;
+    oseRightTarget = 130;
+    int leftOse = encoderGet(driveLeftOse);
+    int rightOse = encoderGet(driveRightOse);
+
+    while ( gyroGet( gyro ) < gyroTarget) {
+
+      leftTargetDistance = oseLeftTarget - ( encoderGet(driveLeftOse) - leftOse ); // gives distance + direction to target. If 0, at target
+      rightTargetDistance = oseRightTarget - ( encoderGet(driveRightOse) - rightOse ); //gives distance to target. If 0, at target.
+      /*neg oseTarget means motors need to drive backwards.
+      target distance neg means go back. Should be same sign as oseTarget
+      target distance pos means go forward. Should be same sign as oseTarget
+      */
+      leftPercent = leftTargetDistance / oseTarget; //at start, should be 1 (distance = target). At finish, 0 (target distance = 0). Gets smaller and smaller
+      rightPercent = rightTargetDistance / oseTarget; //will give us a direction because oseTarget tells us cw/ccw.
+      //map this percentage from 0-1 on function
+      //use function: y=-(2/3)(2x-1)^2 + 1 and then multiply by 100 to get motor value?
+      setDriveLeft( ( ( -2 / 3 ) * ( 2 * ( (leftPercent) - 1 ) ^ 2 ) + 1 ) * 100 );
+      setDriveRight( ( ( -2 / 3 ) * ( 2 * ( (rightPercent) - 1 ) ^ 2 ) + 1 ) * 100 );
+    }
+
+    //Victor
+    while( abs(encoderGet(driveLeftOse) - oseTarget) <= 5 && abs(encoderGet(driveRightOse) - oseTarget) <= 5) {
+
+        Ldeviation = encoderGet(driveLeftOse) - oseTarget;
+        Rdeviation = encoderGet(driveRightOse) + oseTarget;
+
+        leftPercent = encoderGet(driveLeftOse) / oseTarget;
+        rightPercent = encoderGet(driveRightOse) / oseTarget;
+
+        rightPower = ( ( -2 / 3 ) * ( 2 * ( (rightPercent) - 1 ) ^ 2 ) + 1 ) * 100;
+        leftPower = ( ( -2 / 3 ) * ( 2 * ( (leftPercent) - 1 ) ^ 2 ) + 1 ) * 100 ;
+
+        if(abs(Ldeviation) <= 5)
+          setDriveLeft((abs(Ldeviation)/Ldeviation)*rightPower);
+        else
+          setDriveLeft(0);
+
+        if(abs(Rdeviation) <= 5)
+          setDriveRight((abs(Rdeviation)/Rdeviation)*leftPower);
+        else
+          setDriveRight(0);
+
+    }
+
+  } else if( turnType == 2 ) {
+
+  } else if( turnType == 3 ) {
+
+  } else if( turnType == 4 ) {
 
   }
+
+
+
 
 }
