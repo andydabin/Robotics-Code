cbb005497f71b0f381f2bf86e4bcd6c2d5013ca6
Victor Wang <45219858+vdoubleu@users.noreply.github.com>
2018-11-22 18:46:54 -0500

    Update init.c

--- src/init.c
+++ src/init.c
@@ -7,7 +7,7 @@ void initializeIO() {
 void initialize() {
 
   flyOSE = encoderInit(OSE_FLYWHEEL_TOP, OSE_FLYWHEEL_BOTTOM, NOT_REVERSED);
-  driveLeftOse = encoderInit(OSE_DRIVE_LEFT_TOP, OSE_DRIVE_LEFT_BOTTOM, NOT_REVERSED);
+  driveLeftOse = encoderInit(OSE_DRIVE_LEFT_TOP, OSE_DRIVE_LEFT_BOTTOM, REVERSED);
   driveRightOse = encoderInit(OSE_DRIVE_RIGHT_TOP, OSE_DRIVE_RIGHT_BOTTOM, NOT_REVERSED);
   gyro = gyroInit(GYRO_INPUT, 0);
   gyroReset( gyro );
